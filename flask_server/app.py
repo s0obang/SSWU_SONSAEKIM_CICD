844cbf4bba673aa065f4228051aa6fd9ea326909
s0obang <tnqls40168@gmail.com>
2025-12-02 19:08:11 +0900

    debug

--- flask_server/app.py
+++ flask_server/app.py
@@ -23,4 +23,4 @@ def predict():
     return jsonify({'predicted': predicted, 'confidence': confidence})
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5001)
+    app.run(host='0.0.0.0', port=5000)
